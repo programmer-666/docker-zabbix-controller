1523822784a28a0136ea87c6fbf39c3055bc95cc
Süha <suharslan@protonmail.ch>
2022-07-21 09:45:07 +0300

    Add files via upload

---
+++ backup-control/zabbix-container-backup.bat
@@ -0,0 +1,19 @@
+del %cd%\*.tar
+msg %username% Zabbix containers are backing up, please dont use terminals.
+
+docker commit zabbix-web-nginx-mysql zabbix-web-nginx-mysql:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+docker commit zabbix-server-mysql zabbix-server-mysql:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+docker commit zabbix-java-gateway zabbix-java-gateway:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+docker commit mysql-server mysql-server:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+
+docker save zabbix-web-nginx-mysql:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2% -o %cd%\zabbix-web-nginx-mysql.tar
+docker save zabbix-server-mysql:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2% -o %cd%\zabbix-server-mysql.tar
+docker save zabbix-java-gateway:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2% -o %cd%\zabbix-java-gateway.tar
+docker save mysql-server:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2% -o %cd%\mysql-server.tar
+
+docker rmi zabbix-web-nginx-mysql:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2% zabbix-server-mysql:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2% zabbix-java-gateway:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2% mysql-server:V1B%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+
+7z a -tzip %cd%\%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%.zip %cd%\zabbix-web-nginx-mysql.tar %cd%\zabbix-server-mysql.tar %cd%\zabbix-java-gateway.tar %cd%\Zabbix\mysql-server.tar
+del %cd%\*.tar
+
+msg %username% Program completed.
